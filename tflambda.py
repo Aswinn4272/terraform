835520fbd3bdd4b53c1e4cce64a3dc37cd269182
Aswinn4272 <130442173+Aswinn4272@users.noreply.github.com>
2023-05-04 16:46:11 +0530

    Add files via upload

---
+++ tflambda.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    s3 = boto3.resource('s3')
+
+    bucket = s3.Bucket('aswin-tf-test-bucket')
+    dest_bucket=s3.Bucket('aswin-2-bucket')
+    
+    print(dest_bucket)
+    print(bucket)
+     
+    for obj in bucket.objects.filter(Prefix='images/',Delimiter='/'):
+       dest_key=obj.key
+       print(dest_key)
+       print('copy file ' + dest_key)
+       s3.Object(dest_bucket.name, dest_key).copy_from(CopySource= {'Bucket': obj.bucket_name, 'Key': obj.key})
+       print('delete file from source bucket ' + dest_key)
+       s3.Object(bucket.name, obj.key).delete()
+    
